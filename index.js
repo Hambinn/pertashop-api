7f4e3e582c6364fd174dd00ef3b3e36937ef8a15
Hambinn <ilhambintangn@gmail.com>
2022-12-20 00:25:40 +0700

    connecting to firestore

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKeys.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+const db = admin.firestore();
+
+let customers = db.collection('Customers');
+
+customers.get().then((snapshot) => {
+  snapshot.forEach(document =>{
+    console.log(document.id, '=>', document.data());
+  })
+})
